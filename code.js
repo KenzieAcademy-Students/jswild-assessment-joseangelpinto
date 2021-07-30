459b454b44302d2a894f8ed0e040d59d31e79ef5
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-07-30 00:40:25 +0000

    deploy: ea27592b6fa74a0be4dae930eeb012e3391c5807

--- code.js
+++ code.js
@@ -1,57 +1,65 @@
-// To run this assignment, right click on index.html in the Visual Studio file explorer to the left
-// and select "Open with Live Server"
+/ To run this assignment, right click on index.html in the Visual Studio file explorer to the left
+// and select "Open with Live Server"
 
-// Your Code Here.
-//  let position = navigator.geolocation.getCurrentPosition(handlePosition) 
-//  console.log(position)
-//  function handlePosition (){
-//      console.log(position)
-// }
+// Your Code Here.
+let position = {
+    latitude: 0,
+    longitude: 0
+};
 
+navigator.geolocation.watchPosition(success);
 
-let currentLocation;
-let page =1;
-let perPage = 1;
-let term = "dog";
+function success(pos){
+    const crd = pos.coords;
 
-function showMylocation(){
-    if('geolocation' in navigator){
-        navigator.geolocation.getCurrentPosition(function(position){
-            console.log(position.coords.latitude);
-            console.log(position.coords.longitude);
-        });
-    }
+    if (
+        position.latitude !== crd.latitude || 
+        position.longitude !== crd.longitude 
+    
+    ) {
+        position.latitude = crd.latitude;
+        position.longitude = crd.longitude; 
+        console.log("Your Position has changed");
+        console.log(`lat: ${position.latitude} lon: ${position.longitude}`);
+    } else {
+        console.log("Your position remains the same.");
+    }    
 }
-showMylocation();
+   
+const key= '69eb5574217b003d0bc661517e744f6d';
+// secret: d0f2006f8f7f413d
 
-function locationSuccess(data){
-    currentLocation = data.coords;
-    console.log(data.coords)
-    console.log(currentLocation)
-    console.log(generateApiUrl())
-    fetch(generateApiUrl())
-    .then(response=>response.json())
-    .then(data=>console.log(data))
-}
+let queryURL = `https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=${key}&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=${position.latitude}&lon=${position.longitude}&text=dog`;
 
-function locationError(){
-    let backLocation = {latitude: 35.7859328, longitude:-86.409216} // Murfreesboro TN
-} 
+function constructImageURL (photoObj) {
+    return (
+         "https://farm" + 
+         photoObj.farm +
+        ".staticflickr.com/" +
+         photoObj.server +
+         "/" +
+         photoObj.id + 
+         "_" +
+         photoObj.secret +
+           ".jpg"
+    );       
+}            
 
-function generateApiUrl() {
-    return 'https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/' +
-    '?api_key=97022c3ec5d629b999b722e9bd140543' +
-    '&format=json' +
-    '&nojsoncallback=1' +
-    '&method=flickr.photos.search' +
-    '&safe_search=1' +
-    '&per_page=' + perPage +
-    '&page =' + page +
-    '&text' + term +
-    '&lat=' + currentLocation.latitude + 
-    '&lon=' + currentLocation.longitude;
-
-}
-
-navigator.geolocation.getCurrentPosition(locationSuccess, locationError)
-navigator.geolocation.watchPosition(locationSuccess,locationError)
+fetch(queryURL)
+.then((response) => response.json())
+.then ((data) => {
+    console.log(data);
+    let img = document.createElement("img");
+    document.body.append(img);
+    let i = 0;
+    setInterval(function() {
+        // change the pic the next is teh array
+        const imageUrl = constructImageURL(data.photos.photo[i]);
+        img.src = imageUrl;
+        if(i + 1 === data.photos.photo.length){
+           i = 0
+        } else {
+        i++;
+        }
+    }, 2000);
+});
